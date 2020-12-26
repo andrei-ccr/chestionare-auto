f1b83d11fe93b081869a9d877c48c0143b85355f
Andrei Ciucur <ciucur.andrei@gmail.com>
2020-12-26 22:09:31 +0200

    Delete some unnecessary files

--- connect.php
+++ connect.php
@@ -1,16 +1,22 @@
 <?php
-	//Connect to database
-	/*$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	/** Database connection credentials */
 	
-	define("DB_SERVER", $cleardb_url["host"]);
-	define("DB_USER", $cleardb_url["user"]);
-	define("DB_PASS", $cleardb_url["pass"]);
-	define("DB_NAME", substr($cleardb_url["path"],1));*/
-
-
+	//Comment the line below to connect using ClearDB credentials 
+	define("DEV", 1, true);
 	
-	define("DB_SERVER", "127.0.0.1");
-	define("DB_USER", "root");
-	define("DB_PASS", "");
-	define("DB_NAME", "chestionare");
+	if(defined("DEV")) {
+		define("DB_SERVER", "127.0.0.1");
+		define("DB_USER", "root");
+		define("DB_PASS", "");
+		define("DB_NAME", "chestionare");
+	}
+	else {
+		$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+		
+		define("DB_SERVER", $cleardb_url["host"]);
+		define("DB_USER", $cleardb_url["user"]);
+		define("DB_PASS", $cleardb_url["pass"]);
+		define("DB_NAME", substr($cleardb_url["path"],1));
+	}
+	
 ?>
